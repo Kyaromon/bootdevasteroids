d13d7ea21f2906b7c8121852b7e0b37ff6eaf6f8
Eternimus <email4bootdev@proton.me>
2025-11-17 12:27:19 -0900

    B: add main.py, spacing error

--- main.py
+++ main.py
@@ -6,18 +6,24 @@ pygame.init()
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 def main():
-    print("Starting Asteroids with pygame version: VERSION")
+    print("Starting Asteroids with pygame version:", pygame.version.ver)
     print(f"Screen width: {SCREEN_WIDTH}")
-    print(F"Screen height: {SCREEN_HEIGHT}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
 
+    #loop start
     running = True
     while running:
-	log_state()
-	for event in pygame.event.get():
-	    if event.type == pygame.QUIT
-		running = False
-	screen.fill("black")
-	pygame.display.flip()
+        log_state()
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+        screen.fill("black")
+
+        pygame.display.flip()
+
+    #loop end
 
 if __name__ == "__main__":
     main()
